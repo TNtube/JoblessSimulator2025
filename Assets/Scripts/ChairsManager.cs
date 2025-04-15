702345dd83ba31cb13652d12c01b429f48fe3b76
Ned <67144261+Ne-d@users.noreply.github.com>
2025-04-15 15:05:32 +0200

    Add NPC movement

--- Assets/Scripts/ChairsManager.cs
+++ Assets/Scripts/ChairsManager.cs
@@ -6,13 +6,16 @@ public class ChairsManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject chairPrefab;
+
+    [SerializeField]
+    private GameObject npcPrefab;
     
     [SerializeField]
     private int chairsNumber = 5;
     
     
     List<GameObject> _chairs = new List<GameObject>();
-    List<NPCBehaviour> _npcs = new List<NPCBehaviour>();
+    List<GameObject> _npcs = new List<GameObject>();
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -64,18 +67,42 @@ public class ChairsManager : MonoBehaviour
 
     void UpdateNPCsTarget()
     {
+        while(_npcs.Count < chairsNumber)
+            _npcs.Add(Instantiate(npcPrefab, transform.position, transform.rotation));
+        
+        while (_npcs.Count > chairsNumber)
+            _npcs.RemoveAt(_npcs.Count - 1);
+        
         var slots = chairsNumber + 1;
         var angle = 360 / slots;
         var depth = Vector3.Distance(transform.position, _chairs[0].transform.position);
 
         int index = 0;
-        foreach (var chair in _chairs)
+        foreach (var npc in _npcs)
         {
             var rotation = Quaternion.Euler(0, angle * index, 0);
             var direction = rotation * Vector3.forward;
             
-            chair.transform.LookAt(transform.position + direction * 100);
-            chair.transform.position = transform.position + direction.normalized * (depth + depth / 3.0f); // magic offset
+            npc.transform.LookAt(transform.position + direction * 100);
+            npc.transform.position = transform.position + direction.normalized * (depth + depth / 3.0f); // magic offset
+            index++;
+        }
+    }
+
+    void UpdateNPCsPosition()
+    {
+        var slots = chairsNumber + 1;
+        var angle = 360 / slots;
+        var depth = Vector3.Distance(transform.position, _chairs[0].transform.position);
+
+        int index = 0;
+        for (int i = 0; i < _npcs.Count; i++)
+        {
+            var rotation = Quaternion.Euler(0, angle * index + Time.time, 0);
+            var direction = rotation * Vector3.forward;
+
+            _npcs[i].transform.LookAt(transform.position + direction * 100);
+            _npcs[i].transform.position = transform.position + direction.normalized * (depth + depth / 3.0f); // magic offset
             index++;
         }
     }
@@ -109,6 +136,6 @@ public class ChairsManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateNPCsPosition();
     }
 }
