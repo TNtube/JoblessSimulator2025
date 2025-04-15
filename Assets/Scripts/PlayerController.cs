71429a267f584b9872ee1717e244c92031369819
Ned <67144261+Ne-d@users.noreply.github.com>
2025-04-15 12:07:00 +0200

    Implement Crouching detection and UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Camera m_camera;
+    [SerializeField] private Text m_currentHeightText;
+    [SerializeField] private Text m_currentStateText;
     
     [SerializeField] private float m_standingHeight;
     [SerializeField] private float m_crouchingHeight;
@@ -11,13 +14,13 @@ public class PlayerController : MonoBehaviour
 
     public void CalibrateStandingHeight()
     {
-        m_standingHeight = m_camera.transform.position.y;
+        m_standingHeight = GetHeight();
         ComputeCrouchingThreshold();
     }
 
     public void CalibrateCrouchingHeight()
     {
-        m_crouchingHeight = m_camera.transform.position.y;
+        m_crouchingHeight = GetHeight();
         ComputeCrouchingThreshold();
     }
 
@@ -26,14 +29,19 @@ public class PlayerController : MonoBehaviour
         m_crouchingThreshold = (m_standingHeight + m_crouchingHeight) / 2;
     }
 
+    private float GetHeight() => m_camera.transform.position.y;
+    
     public bool IsSitting()
     {
         return m_camera.transform.position.y < m_crouchingThreshold;
     }
-
+    
     private void Update()
     {
         if (IsSitting())
             Debug.Log("Sitting.x");
+
+        m_currentHeightText.text = GetHeight().ToString();
+        m_currentStateText.text = IsSitting() ? "Crouching" : "Standing";
     }
 }
